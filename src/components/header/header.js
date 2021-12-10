c7ffc850a1d13cd9f0cab50b4864f6168fb27357
Alexander <caminante.msk@gmail.com>
2021-12-11 01:22:39 +0300

    added header scroll fixed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,2 +1,14 @@
 
 console.log ('Hola! Soy Header');
+
+import $ from 'jquery';
+
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 1) {
+    $(".header, .header__logo, .header__subtitle, .header__title, .header__nav").addClass("scroll"); 
+  }
+  else {
+    $(".header, .header__logo, .header__subtitle, .header__title, .header__nav").removeClass("scroll");
+  }
+});
